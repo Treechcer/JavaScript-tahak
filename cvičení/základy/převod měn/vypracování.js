4da1ab2096017f0a88697fec59054d1f41f91f6c
Treechcer <150960915+Treechcer@users.noreply.github.com>
2025-03-03 14:56:27 +0100

    Create vypracování.js

---
+++ cvičení/základy/převod měn/vypracování.js
@@ -0,0 +1,5 @@
+var EUR = 10; //protože ještě neznáme způsob vstupu uživatele musíme to nastavené na tvrdo
+
+var CZK = EUR * 25.08; //musíme to vynásobit číslem 25.08 protože ve chvíli psaní je jedno euro 25.08 Kč, toto proměnnou uloží a my s ní můžeme dělat další věci.
+
+console.log(CZK); // zde vypíšeme proměnnou lze použít i document.write() aby to bylo vidět v HTML souboru ale to si myslím že není důležité a bude to mít podobný efekt.
