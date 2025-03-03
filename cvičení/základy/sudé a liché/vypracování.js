11b2ba7f679f0e71715935c8405fb58bc5483dcd
Treechcer <150960915+Treechcer@users.noreply.github.com>
2025-03-03 15:44:25 +0100

    Create vypracování.js

---
+++ cvičení/základy/sudé a liché/vypracování.js
@@ -0,0 +1,7 @@
+var cislo = 5; // číslo, u kterého zjistíme jestli je sudé nebo liché
+
+var vysledek = cislo % 2; // zjistíme celo číselný zbytek čísla, více že když šílo je dělitelné 2 (2,4,8..) tak je sudé a když není (1,3,5..) je liché
+
+console.log(vysledek); // vypíše to 1 nebo 0 podle toho jestli je sudé nebo liché, 0 sudé 1 liché
+
+//v tomto případě je výsledek 1 (sudé). Zase se ke kódu vvrátíme později.
