4bfe07b2f5e573063a3ccef2f05949eac9e92e7f
Treechcer <150960915+Treechcer@users.noreply.github.com>
2025-03-05 10:53:16 +0100

    Update zadání.js

--- cvičení/seznamy/největší číslo v seznamu/zadání.js
+++ cvičení/seznamy/největší číslo v seznamu/zadání.js
@@ -1,1 +1,12 @@
-udělej kód, kde bude funkce, která přijme pole plně čísel a vrátí největší číslo z toho pole
+function Max(seznam){ 
+    max = seznam[0]; // vezmeme první prvek a dáme ho do maxima (protože když dáme 0 nebo jiné číslo je možné že by to pole obsahovalo pouze menší a funkce by vrátila špatný výsledek 
+    for (i of seznam){ //projdeme celé pole a uložíme prvky po každé iterací (opakování) do i
+       if (max < i){ // když i je větší jak maximum uložíme ho jako maximum
+           max = i;
+       }
+    }
+    
+    return max; // vrátíme maximum neboli největší číslo 
+}
+
+console.log(Max([1,2,3,9,2])) // vypíše 9
