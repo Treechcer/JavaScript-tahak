4112029f74f453481f4a73c32b5b5f9ad6d75163
Treechcer <150960915+Treechcer@users.noreply.github.com>
2025-03-19 19:48:43 +0100

    Update vypracované.js

--- cvičení/cykly/Leibnizova řada (odhad pí (π))/vypracované.js
+++ cvičení/cykly/Leibnizova řada (odhad pí (π))/vypracované.js
@@ -1,11 +1,11 @@
 var pi = 1 // musíme začíná na jedné protože řada žácíná 1-1/3..
-isPlus = false // začínáme na flade protože nechceme sčítat jako první 
+isPlus = false // začínáme na false protože nechceme sčítat jako první 
 
 for (let i = 3; i <= 50000000; i+=2){ // tady jdeme od 3 (první číslo v řadě po 1) do 10000 (může to být jakkoliv jiné číslo jen čím je menší tím má i menší přesnost)
     if (isPlus){ // pokud isPlus je true tak budeme sčítat 
         pi += 1/i //protože sčítáme zlomek 1/n potřebujeme ho tady sčíst
     }
-    else{ // v tuto chvíli víme že isPkus musí být falše s proto budeme odečítat 
+    else{ // v tuto chvíli víme že isPkus musí být false s proto budeme odečítat 
         pi -= 1/i //protože odčítáme zlomek 1/n potřebujeme ho tady odečíst 
     }
     isPlus = !isPlus //obrácení polarity isPlus (true na false a false na true)
